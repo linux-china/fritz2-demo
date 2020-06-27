431c4e67ba5256919e3e631b74cc0c7c55e646e5
linux_china <libing.chen@gmail.com>
2020-06-27 14:25:01 -0700

    polishing

--- src/jsMain/kotlin/app.kt
+++ src/jsMain/kotlin/app.kt
@@ -1,10 +1,32 @@
+import dev.fritz2.binding.RootStore
+import dev.fritz2.binding.handledBy
 import dev.fritz2.dom.html.render
 import dev.fritz2.dom.mount
+import dev.fritz2.dom.values
+import kotlinx.coroutines.flow.map
 
 fun main() {
-    render {
-        p {
-            text("Hello World!")
+    val store = object : RootStore<String>("Your Name") {
+        val clear = handle { model ->
+            console.log("Clearing")
+            ""
         }
+    }
+
+    render {
+        div {
+            p {
+                store.data.map { "input: $it" }.bind()
+            }
+            input {
+                value = store.data
+                changes.values().handledBy(store.update)
+            }
+            button {
+                text("Clear")
+                clicks handledBy store.clear
+            }
+        }
+
     }.mount("target")
 }
